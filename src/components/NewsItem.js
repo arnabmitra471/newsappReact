a0b58918c1568b625930c00a584fbf490e10faaa
Arnab <arnabmitra471@gmail.com>
2023-09-16 12:35:02 +0530

    Understanding props object and state object

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
+  constructor(){
+    super();
+    console.log("Hello !! I am a constructor from NewsItem component");
+  }
   render() {
-    let {title,description} = this.props;
+    let {title,description,imageUrl} = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsitem" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsitem" className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
       </div>

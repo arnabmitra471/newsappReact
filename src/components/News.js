9d5933475309690cb9996059abf49b03900c9ee0
Arnab <arnabmitra471@gmail.com>
2023-09-16 09:52:31 +0530

    Created grid layout for news items

--- src/components/News.js
+++ src/components/News.js
@@ -4,10 +4,23 @@ import NewsItem from './NewsItem';
 export default class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem/>
+      <div className="container">
+        <h2>NewsMonkey -top headlines</h2>
+        <div className="row">
+          <div className="col-md-3">
+
+        <NewsItem title="This is a sample title1" description ="This is the description1"/>
+          </div>
+          <div className="col-md-3">
+
+        <NewsItem title="This is a sample title2" description ="This is the description2"/>
+          </div>
+          <div className="col-md-3">
+
+        <NewsItem title="This is a sample title3" description ="This is the description3"/>
+          </div>
       </div>
+    </div>
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
+    let {title,description} = this.props;
     return (
-      <div>This is a news item</div>
+      <div>
+        <div className="card" style={{width: "18rem"}}>
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsitem" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     )
   }
 }

92c930402fb7ad684b81dc1725e085da154dd2c2
Arnab <arnabmitra471@gmail.com>
2023-09-23 22:50:40 +0530

    created updateNews() function and fetched data

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@ export default class App extends Component {
           <Navbar />
 
           <Routes>
-            <Route exact path="/" element={<News  key="general" pageSize={20} country="in" category="general" />}></Route>
-            <Route exact path="/business"element={<News  key="business" pageSize={20} country="in" category="business" />}></Route>
+            <Route exact path="/" element={<News  key="general" pageSize={this.pageSize} country="in" category="general" />}></Route>
+            <Route exact path="/business"element={<News  key="business" pageSize={this.pageSize} country="in" category="business" />}></Route>
             <Route exact path="/entertainment" element={<News  key="entertainment" pageSize={20} country="in" category="entertainment" />}></Route>
-            <Route exact path="/general" element={<News  key="general" pageSize={20} country="in" category="general" />}></Route>
-            <Route exact path="/health" element={<News  key="health" pageSize={20} country="in" category="health" />}></Route >
-            <Route exact path="/science" element={<News  key="science" pageSize={20} country="in" category="science" />}> </Route >
-            <Route exact path="/sports" element={<News  key="sports" pageSize={20} country="in" category="sports" />}></Route >
-            <Route exact path="/technology" element={<News  key="technology" pageSize={20} country="in" category="technology" />}></Route >
+            <Route exact path="/general" element={<News  key="general" pageSize={this.pageSize} country="in" category="general" />}></Route>
+            <Route exact path="/health" element={<News  key="health" pageSize={this.pageSize} country="in" category="health" />}></Route >
+            <Route exact path="/science" element={<News  key="science" pageSize={this.pageSize} country="in" category="science" />}> </Route >
+            <Route exact path="/sports" element={<News  key="sports" pageSize={this.pageSize} country="in" category="sports" />}></Route >
+            <Route exact path="/technology" element={<News  key="technology" pageSize={this.pageSize} country="in" category="technology" />}></Route >
           </Routes >
         </Router >
       </div >
--- src/components/News.js
+++ src/components/News.js
@@ -4,6 +4,7 @@ import LoadingSpinner from './LoadingSpinner';
 import PropTypes from 'prop-types';
 
 export default class News extends Component {
+  pageSize = 20
   articles = [
     {
       "source": {
@@ -267,90 +268,66 @@ export default class News extends Component {
     }
   ]
   static propTypes = {
-      country:PropTypes.string,
-      pageSize:PropTypes.number,
-      category:PropTypes.string
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
   };
   static defaultProps = {
-      country:"ar",
-      pageSize:10,
-      category:"science"
+    country: "ar",
+    pageSize: 10,
+    category: "science"
 
   }
-  constructor(){
+  constructor() {
     super();
     //console.log("Hello I am a constructor from news component");
     this.state = {
       articles: this.articles,
-      loading:false,
-      page:1
+      loading: false,
+      page: 1
     }
-  }
-  async componentDidMount(){
-    console.log("cdm");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=112ab18126964b80b648149d0f4def2b&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true})
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({articles:parsedData.articles,
-      totalResults:parsedData.totalResults,
-      loading:false
-    })
-  }
-  handlePrevClick = async ()=>{
-    console.log("Previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=112ab18126964b80b648149d0f4def2b&page=${this.state.page - 1 }&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true})
+  } 
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=112ab18126964b80b648149d0f4def2b&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true })
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
-      page:this.state.page - 1,
-      articles:parsedData.articles,
-      loading:false
-
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false
     })
   }
-  handleNextClick = async ()=>{
-    console.log("Next");
-    if(this.state.page + 1> Math.ceil(this.state.totalResults/this.props.pageSize))
-    {
-
-    }
-    else
-    {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=112ab18126964b80b648149d0f4def2b&page=${this.state.page + 1 }&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true})
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      page:this.state.page + 1,
-      articles:parsedData.articles,
-      loading:false
-
-    })
+  async componentDidMount() {
+      this.updateNews();
   }
+  handlePrevClick = async () => {
+    this.setState({page:this.state.page -1})
+    this.updateNews();
+  }
+  handleNextClick = async () => {
+    this.setState({page:this.state.page +1})
   }
   render() {
     console.log("render");
     return (
       <div className="container">
-        <h2 className="text-center" style={{margin: '40px'}}>NewsMonkey -top headlines</h2>
-        {this.state.loading && <LoadingSpinner/>}
+        <h2 className="text-center" style={{ margin: '40px' }}>NewsMonkey -top headlines</h2>
+        {this.state.loading && <LoadingSpinner />}
         <div className="row">
-        {!this.state.loading && this.state.articles.map((element)=>{
+          {!this.state.loading && this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
-          
-              <NewsItem title={element != null ? element.title.slice(0,45):""} description={element.description != null ? element.description.slice(0,88):element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
-              
+
+              <NewsItem title={element != null ? element.title.slice(0, 45) : ""} description={element.description != null ? element.description.slice(0, 88) : element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+
             </div>
-            })};
-          </div>
-          <div className="container d-flex justify-content-between">
-          <button disabled={this.state.page<=1}type="button" className="btn btn-danger" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-danger" onClick={this.handleNextClick}>Next &rarr;</button>
-          </div>
-    </div>
+          })};
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-danger" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-danger" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
+      </div>
     )
   }
 }
